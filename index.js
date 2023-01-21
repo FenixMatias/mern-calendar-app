e5db0266306bdb0c683cb3ee790ecb0bb84da295
FenixMatias <matiaszodiaco@gmail.com>
2023-01-21 14:39:56 -0300

    Se prepara despliegue a producción v7

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 //TODO: CRUD: Eventos
 app.use('/api/events', require('./routes/event'));
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     // res.sendFile(__dirname + '/public/index.html');
-    res.sendFile('index.html', {root: path.join(__dirname, '/public/index.html')});
+    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 });
 
 
